0935e409dc1a5867904e30b8946009a6e2eefd14
Eddie Kim <eddienakkim@gmail.com>
2020-07-07 20:45:32 -0700

    updated folders

--- 14-handlebars/01-Activities/06-HandlebarsAnimals/Unsolved/server.js
+++ 14-handlebars/01-Activities/06-HandlebarsAnimals/Unsolved/server.js
@@ -1,11 +1,8 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
-
 var app = express();
+var port = 3000;
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -36,27 +33,33 @@ var animals = [
 
 app.get("/dog", function(req, res) {
   // Handlebars requires an object to be sent to the dog.handlebars file. Lucky for us, animals[0] is an object!
+   res.render("dog", animals[0]);
 
   // 1. Send the dog object from the animals array to the dog.handlebars file.
 
 });
 
+
 app.get("/all-pets", function(req, res) {
   // Handlebars requires an object to be sent to the index.handlebars file.
+  res.render("index", { index: animals });
 
   // 2. Send the animals to the index.handlebars file. Remember that animals is an array and not an object.
 
 });
 
 app.get("/all-non-pets", function(req, res) {
-  // Handlebars requires an object to be sent to the index.handlebars file.
 
+  let notPetArr = [];
+  // Handlebars requires an object to be sent to the index.handlebars file.
+  for (var i = 0; i < animals.length; i++) {
+    if (animals[i].pet == false) {
+      notPetArr.push(animals[i]);
+    }
+  }
+  res.render("index", { index : notPetArr });
   // 3. Send all the animals that are not pets to the index.handlebars file.
 
 });
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+app.listen(port);
