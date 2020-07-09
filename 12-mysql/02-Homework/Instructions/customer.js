ac9a1e37e91f70e9e07a853fa6619b334183199e
Eddie Kim <eddienakkim@gmail.com>
2020-07-09 10:40:53 -0700

    final

--- 12-mysql/02-Homework/Instructions/customer.js
+++ 12-mysql/02-Homework/Instructions/customer.js
@@ -49,3 +49,123 @@ let displayItems = function() {
     });
 };
 
+// function to prompt user to enter ID of the product to purchase
+let askForID = function() {
+    inquirer.prompt({
+        name: 'itemID',
+        type: 'input',
+        message: 'Enter the ID of the item you would like to purchase:',
+        // validate input is number from 1-10
+        validate: (value) => {
+            if (!isNaN(value) && (value > 0 && value <= 10)) {
+                return true;
+            } else {
+                console.log(chalk.red(' => Please enter a number from 1-10'));
+                return false;
+            }
+        }
+    // select all rows where ID = user's input
+    }).then((answer) => {
+        connection.query('SELECT item_id, product_name, price, stock_quantity, product_sales FROM products WHERE ?', { item_id: answer.itemID }, (err, res) => {
+            // confirm with user that this is the product they'd like to purchase
+            confirmItem(res[0].product_name, res);
+        });
+    });
+};
+
+// function to confirm with user that the product they chose is correct
+let confirmItem = function(product, object) {
+    inquirer.prompt({
+        name: 'confirmItem',
+        type: 'confirm',
+        message: `You chose` + chalk.blue.bold(` '${product}'. `) + `Is this correct?`
+    }).then((answer) => {
+        if (answer.confirmItem) {
+            chosenItem = {
+                item_id: object[0].item_id,
+                product_name: object[0].product_name,
+                price: object[0].price,
+                stock_quantity: object[0].stock_quantity,
+                product_sales: object[0].product_sales
+            };
+            // ask how many they'd like to purchase
+            askHowMany(chosenItem.item_id);
+        } else {
+            askForID();
+        }
+    });
+};
+
+// function to ask user how many of the products they'd like to purchase
+let askHowMany = function(chosenID) {
+    inquirer.prompt({
+        name: 'howMany',
+        type: 'input',
+        message: 'How many would you like to purchase?',
+        validate: (value) => {
+            if (!isNaN(value) && value > 0) {
+                return true;
+            } else {
+                console.log(chalk.red(' => Oops, please enter a number greater than 0'));
+                return false;
+            }
+        }
+    }).then((answer) => {
+        connection.query('SELECT stock_quantity FROM products WHERE ?', { item_id: chosenItem.item_id }, (err, res) => {
+            // if there are not enough products in stock
+            if (res[0].stock_quantity < answer.howMany) {
+                console.log(chalk.blue.bold('\n\tSorry, insufficient quantity in stock!\n'));
+                // confirm if user would still like to buy this product
+                inquirer.prompt({
+                    name: 'proceed',
+                    type: 'confirm',
+                    message: 'Would you still like to purchase this product?'
+                }).then((answer) => {
+                    if (answer.proceed) {
+                        askHowMany(chosenItem.item_id);
+                    } else {
+                        console.log(chalk.blue.bold('\n\tThanks for visiting! We hope to see you again soon.\n'));
+                        connection.end();
+                    }
+                });
+            // if there are enough products in stock for purchase to go through
+            } else {
+                chosenItem.howMany = answer.howMany;
+                console.log(chalk.blue.bold('\n\tOrder processing...'));
+                // console.log(chosenItem);
+
+                // update database to reflect new stock quantity after sale
+                connection.query('UPDATE products SET ? WHERE ?', [
+                    {
+                        stock_quantity: chosenItem.stock_quantity - answer.howMany,
+                        product_sales: chosenItem.product_sales + (chosenItem.price * answer.howMany)
+                    },
+                    {
+                        item_id: chosenItem.item_id
+                    }
+                ], (err, res) => {
+                    console.log(chalk.blue.bold(`\n\tOrder confirmed!!! Your total was $${(chosenItem.price * chosenItem.howMany).toFixed(2)}.\n`));
+                    // ask if user would like to make another purchase
+                    promptNewPurchase();
+                });
+            }
+        });
+    });
+}
+
+// function to ask if user would like to make another purchase
+let promptNewPurchase = function() {
+    inquirer.prompt({
+        name: 'newPurchase',
+        type: 'confirm',
+        message: 'Would you like to make another purchase?'
+    }).then((answer) => {
+        if (answer.newPurchase) {
+            resetCart();
+            askForID();
+        } else {
+            console.log(chalk.blue.bold('\n\tWe appreciate your business. Have a great day!\n'));
+            connection.end();
+        }
+    });
+};
