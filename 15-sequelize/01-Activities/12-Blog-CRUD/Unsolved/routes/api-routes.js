469851be8be444bd97ae776257371c93caf3614f
Eddie Kim <eddienakkim@gmail.com>
2020-07-16 21:22:49 -0700

    worked on activities

--- 15-sequelize/01-Activities/12-Blog-CRUD/Unsolved/routes/api-routes.js
+++ 15-sequelize/01-Activities/12-Blog-CRUD/Unsolved/routes/api-routes.js
@@ -8,31 +8,26 @@
 // Requiring our Todo model
 var db = require("../models");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
-
   // GET route for getting all of the posts
   app.get("/api/posts", function(req, res) {
     // Add sequelize code to find all posts, and return them to the user with res.json
-    db.Post.findAll({}).then(function(dbPost) {
-      return res.json(dbPost)
+    db.Post.findAll({}).then(function(dbPost){
+      res.json(dbPost);
     })
   });
-
   // Get route for returning posts of a specific category
   app.get("/api/posts/category/:category", function(req, res) {
     // Add sequelize code to find all posts where the category is equal to req.params.category,
     // return the result to the user with res.json
     db.Post.findAll({
       where: {
-        id: req.params.category
+        category: req.params.category
       }
-    }).then(function(dbPost) {
-      return res.json(dbPost)
+     }).then(function(dbPost){
+      res.json(dbPost);
+    })
   });
-})
-
   // Get route for retrieving a single post
   app.get("/api/posts/:id", function(req, res) {
     // Add sequelize code to find a single post where the id is equal to req.params.id,
@@ -41,24 +36,23 @@ module.exports = function(app) {
       where: {
         id: req.params.id
       }
-    }).then(function(dbPost) {
-      return res.json(dbPost)
+     }).then(function(dbPost){
+      res.json(dbPost);
+    })
   });
-  
-
   // POST route for saving a new post
   app.post("/api/posts", function(req, res) {
     // Add sequelize code for creating a post using req.body,
     // then return the result using res.json
+    console.log(req.body);
     db.Post.create({
       title: req.body.title,
       body: req.body.body,
       category: req.body.category
     }).then(function(dbPost){
-      return res.json(dbPost);
-    });
+      res.json(dbPost);
+    })
   });
-
   // DELETE route for deleting posts
   app.delete("/api/posts/:id", function(req, res) {
     // Add sequelize code to delete a post where the id is equal to req.params.id, 
@@ -68,10 +62,9 @@ module.exports = function(app) {
         id: req.params.id
       }
     }).then(function(dbPost) {
-      return res.json(dbPost);
+      res.json(dbPost);
     });
   });
-
   // PUT route for updating posts
   app.put("/api/posts", function(req, res) {
     // Add code here to update a post using the values in req.body, where the id is equal to
@@ -79,7 +72,7 @@ module.exports = function(app) {
     db.Post.update({
       title: req.body.title,
       body: req.body.body,
-      catergory: req.body.category,
+      category: req.body.category
     }, {
       where: {
         id: req.body.id
@@ -87,7 +80,5 @@ module.exports = function(app) {
     }).then(function(dbPost) {
       return res.json(dbPost);
     });
-  })
-})
-
-}
+  });
+};
